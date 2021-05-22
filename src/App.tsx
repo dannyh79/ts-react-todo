2e96b4b2b8194b24f797e3388486ff5edc947d4a
Danny Cheng-Hsuan Han <chenghsuan.han@gmail.com>
2021-05-22 16:05:59 +0800

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import TodoList from './components/TodoList';
+import BaseForm from './components/BaseForm';
 
-function App() {};
+const todos: ITodoItem[] = [
+  {
+    title: 'Cook dinner',
+    time: new Date(2021, 0o5, 17, 0o0, 0o0),
+    status: 'done',
+    content: '',
+  },
+  {
+    title: 'Get it done',
+    time: new Date(2021, 0o5, 12, 0o0, 0o0),
+    status: 'undone',
+    content: '',
+  },
+];
+
+const App = () => {
+  return (
+    <>
+      <TodoList todos={todos} />
+      <BaseForm />
+    </>
+  );
+};
 
 export default App;
